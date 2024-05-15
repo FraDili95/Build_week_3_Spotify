e5dbcb64f4822a6ee216f960c6b5df8660e38194
Gian Carlo Fandialan <gianchifandialan03@gmail.com>
2024-05-15 20:31:31 +0200

    Creata la funzione fetch

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const RIGHT = -2000; const LEFT = -1000;
 const BUTTON_LEFT = document.getElementById("left");//BOTTONE INDIETRO
 const BUTTON_RIGHT =document.getElementById("right");//BOTTONE AVANTI
 const PAGES = document.querySelectorAll(".page")//ARRAY CONTENENTE I PUNTATORI A TUTTE LE PAGINE
+const albumsContainer = document.getElementById("albumsContainer")//
+const url = "http://striveschool-api.herokuapp.com/api/deezer/search?q=";
 var currentpage = 0;//PAGINA CORRENTE(la imposto a 0 all'inizio che corrisponde alla prima pagina)
 ///////////////////////////////////////////////////////////////////////////////////
 
@@ -31,6 +33,47 @@ function changePage ( arrayPages, direction ){//PER FARLA FUNZIONARE SERVE SOLO 
 
 }//fine funz
 
+//CREO LE FUNZIONE FETCH
+async function fetchAlbum(nomeArtista) {
+    
+    //ESEGUO IL TRY..CATCH
+    try {
+        //Eseguo il fetch
+        const response = await fetch(url+nomeArtista);
+
+        const risultati = await response.json()
+
+        console.log(risultati);
+
+        return risultati
+    } catch (error) {
+        //NAL CASO DI ERRORE LO SEGNALO
+        console.error("Errore:", error)
+    }
+}
+
+// fetchAlbum("queen").then(risultati => {
+//     risultati.data.forEach(risultato => {
+//         console.log(risultato);
+//         const col = document.createElement("div");
+//         col.classList.add("col-3");
+
+//         col.innerHTML =
+//         `
+//         <div class="card" style="width: 18rem;">
+//         <img src="${risultato.album.cover}" class="card-img-top" alt="...">
+//         <div class="card-body">
+//             <h5 class="card-title">${risultato.album.title}</h5>
+//             <p class="card-text">${risultato.artist.name}</p>
+//             <a href="#" class="btn btn-primary">Go somewhere</a>
+//         </div>
+//         </div>
+//         `
+//         albumsContainer.appendChild(col);
+
+
+//     });
+// })
 
 ////////////////////////////////EVENT LISTENERS///////////////////////////////////
 BUTTON_LEFT.addEventListener("click",function(){  changePage(PAGES, LEFT); } );//bottone sx
